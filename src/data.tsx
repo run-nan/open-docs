dc6990cb889980f4e8809e869173b2c67b2154a9
htmlin <linhuangdong@ones.cn>
2023-11-20 16:55:48 +0800

    fix: 修复首页中文翻译问题

--- src/data.tsx
+++ src/data.tsx
@@ -1,39 +1,36 @@
-import React from 'react'
 import Translate, { translate } from '@docusaurus/Translate'
+import React from 'react'
 
 const baseUrl = '/docs/'
-const translateWraperFn = (text: string): string => {
-  // https://docusaurus.io/zh-CN/docs/api/misc/@docusaurus/eslint-plugin/string-literal-i18n-messages
-  return translate({ message: text }) // eslint-disable-line
-}
 
 // 首页导航栏
 export const guideCardData = [
   {
-    title: translateWraperFn('Getting started'),
+    title: translate({ message: 'Getting started' }),
     icon: 'homepage/get-started.svg',
-    description: translateWraperFn(
-      'Before you start working with ONES Open  Platform, learn our guide for more details.'
-    ),
-    tips: translateWraperFn('Learn details'),
+    description: translate({
+      message:
+        'Before you start working with ONES Open  Platform, learn our guide for more details.',
+    }),
+    tips: translate({ message: 'Learn details' }),
     url: 'docs/guide/overview',
   },
   {
-    title: translateWraperFn('API documents'),
+    title: translate({ message: 'API documents' }),
     icon: 'homepage/api-documents.svg',
-    description: translateWraperFn(
-      'Find out what is possible with ONES  API and how to realize data connection.'
-    ),
-    tips: translateWraperFn('Learn details'),
+    description: translate({
+      message: 'Find out what is possible with ONES  API and how to realize data connection.',
+    }),
+    tips: translate({ message: 'Learn details' }),
     url: 'docs/openapi',
   },
   {
-    title: translateWraperFn('Get to know the abilities'),
+    title: translate({ message: 'Get to know the abilities' }),
     icon: 'homepage/open-platform.svg',
-    description: translateWraperFn(
-      'ONES Open Platform provides advanced customization capabilities.'
-    ),
-    tips: translateWraperFn('Learn details'),
+    description: translate({
+      message: 'ONES Open Platform provides advanced customization capabilities.',
+    }),
+    tips: translate({ message: 'Learn details' }),
     url: 'docs/abilities/basic',
   },
 ]
@@ -41,44 +38,46 @@ export const guideCardData = [
 // 能力卡片
 export const abilityCardData = [
   {
-    title: translateWraperFn('Core'),
+    title: translate({ message: 'Core' }),
     icon: 'homepage/capabilities.svg',
-    description: translateWraperFn(
-      'ONES Open Platform provides standard infrastructure for applications.'
-    ),
-    tips: translateWraperFn('See more'),
+    description: translate({
+      message: 'ONES Open Platform provides standard infrastructure for applications.',
+    }),
+    tips: translate({ message: 'See more' }),
     keyword: [
-      translateWraperFn('Database'),
-      translateWraperFn('Notifications'),
-      translateWraperFn('Audit log'),
+      translate({ message: 'Database' }),
+      translate({ message: 'Notifications' }),
+      translate({ message: 'Audit log' }),
     ],
     url: 'docs/abilities/basic',
   },
   {
-    title: translateWraperFn('Slots'),
+    title: translate({ message: 'Slots' }),
     icon: 'homepage/slot.svg',
-    description: translateWraperFn(
-      'Slots enable developers to add, hide, or replace custom content in navigation bars or page elements.'
-    ),
-    tips: translateWraperFn('See more'),
+    description: translate({
+      message:
+        'Slots enable developers to add, hide, or replace custom content in navigation bars or page elements.',
+    }),
+    tips: translate({ message: 'See more' }),
     keyword: [
-      translateWraperFn('System sider bar slot'),
-      translateWraperFn('My work top bar slot'),
-      translateWraperFn('System configuration slot'),
+      translate({ message: 'System sider bar slot' }),
+      translate({ message: 'My work top bar slot' }),
+      translate({ message: 'System configuration slot' }),
     ],
     url: 'docs/abilities/slot',
   },
   {
-    title: translateWraperFn('App Modules'),
+    title: translate({ message: 'App Modules' }),
     icon: 'homepage/app-modules.svg',
-    description: translateWraperFn(
-      'App modules empower developers to carry out extended development of specific features of ONES products and applications.'
-    ),
-    tips: translateWraperFn('See more'),
+    description: translate({
+      message:
+        'App modules empower developers to carry out extended development of specific features of ONES products and applications.',
+    }),
+    tips: translate({ message: 'See more' }),
     keyword: [
-      translateWraperFn('Account: Simple Auth'),
-      translateWraperFn('Project: Custom Script Field'),
-      translateWraperFn('Project: Custom Permission'),
+      translate({ message: 'Account: Simple Auth' }),
+      translate({ message: 'Project: Custom Script Field' }),
+      translate({ message: 'Project: Custom Permission' }),
     ],
     url: 'docs/abilities/business',
   },
